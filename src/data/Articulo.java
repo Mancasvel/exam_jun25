176a8d4a858ab3136762a50f655d30e0a7213b5f
Manuel C. V. <97321235+Mancasvel@users.noreply.github.com>
2025-06-27 11:35:35 +0200

    Initial commit

---
+++ src/data/Articulo.java
@@ -0,0 +1,28 @@
+package data;
+
+import utiles.Checkers;
+
+public record Articulo(String titulo, String categoria, Integer paginas) {
+	
+	public Articulo{
+		Checkers.check("El titulo no puede estar vacio", !(titulo.isBlank()));
+		Checkers.check("El numero de paginas debe ser mayor a 0", paginas > 0);
+		
+	}
+	
+	
+	
+	private static final Integer articuloLargo = 10;
+	
+	public Boolean getArticuloLargo() {
+		Boolean res = false;
+		
+		if(paginas >= articuloLargo) {
+			res = true;
+		}
+		
+		return res;
+
+	}
+	
+}
